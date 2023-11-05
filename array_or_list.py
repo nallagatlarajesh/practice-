2833214a08bc079488859692a48e11c5df38d4d0
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-11-05 16:25:20 +0530

    array as list
    arrays or lists are used to store multiple values in one single variable
    #python has a set of built in methodss that you can use on lists/arrays
    append()
    pop(())
    copy()
    clear()
    count()
    extend()
    index()
    insert()
    remove()
    reverse()
    sort()

---
+++ array_or_list.py
@@ -0,0 +1,13 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+print(cars)
+cars.append("honda")
+print(cars)
+#you can use the pop methode to removee an element from an error 
+cars.pop()
+print(cars)
+cars.pop(1)
+print(cars)
+#delete the element that has the value "FORD "
+cars.remove("Ford")
+print(cars)
